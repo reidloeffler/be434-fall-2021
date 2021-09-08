a36df2582ebc56c76a8115a64493059dbea70d3e
Reid  Loeffler <reidloeffler@dhcp-10-132-212-245.uawifi.arizona.edu>
2021-09-08 09:28:52 -0700

    add salutations.py

---
+++ assignments/01_salutations/salutations.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Author : reidloeffler <reidloeffler@localhost>
+Date   : 2021-09-07
+Purpose: Print greeting
+"""
+
+import argparse
+
+# --------------------------------------------------
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Greetings and salutations',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-g',
+                        '--greeting',
+                        help='The greeting ',
+                        metavar='str',
+                        type=str,
+                        default='Howdy')
+
+    parser.add_argument('-n',
+                        '--name',
+                        help='Whom to greet ',
+                        metavar='str',
+                        type=str,
+                        default='Stranger')
+   
+    parser.add_argument('-e',
+                        '--excited',
+                        help='Include an exclamation point ',
+                        action='store_true')
+
+    return parser.parse_args()
+
+# --------------------------------------------------
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    greeting = args.greeting 
+    name = args.name
+    excited = args.excited
+    
+    if args.excited == 1:
+        excited = '!'
+        
+    if args.excited == 0:
+        excited = '.'
+ 
+    print(greeting+', '+name+excited)
+    
+# --------------------------------------------------
+
+if __name__ == '__main__':
+    main()
