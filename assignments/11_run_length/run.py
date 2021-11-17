3a03c99686b48fec70d358decb105849b4a6c3c8
Reid  Loeffler <reidloeffler@email.arizona.edu>
2021-11-17 12:20:40 -0700

    Added documentation

--- assignments/11_run_length/run.py
+++ assignments/11_run_length/run.py
@@ -33,11 +33,14 @@ def check_input(user_input):
     """ Checks if the user provided a file or a sequence """
 
     if os.path.isfile(user_input):
+        # Checks if the input corresponds to an existing file
         input_type = 'file'
     else:
+        # Estblishes that the file is a sequence if it is not file
         input_type = 'sequence'
 
     return input_type
+    # Returns the input type
 
 
 # --------------------------------------------------
@@ -51,16 +54,26 @@ def build_output(sequence):
     output = ''
 
     for char in sequence:
+        # Iterates over each character in the sequence
+
         if char == prev_char:
             count += 1
+            # Counts the number of occurrences if character is repeated
+
         else:
             output += prev_char
             if count != 1:
                 output += str(count)
             count = 1
+            # Adds the previous character to the output string
+            # Adds the number occurrences to the output string
+            # Resets count to 1
+
         prev_char = char
+        # Reassigns the previous character value to the current character
 
     return output
+    # Returns the compressed sequence
 
 
 # --------------------------------------------------
@@ -74,13 +87,22 @@ def main():
     input_type = check_input(user_input)
 
     if input_type == 'sequence':
+        # Checks if the input is a string/sequence
+
         sequence = user_input + ' '
         print(build_output(sequence))
+        # Passes the string/sequence in to the build_ouput function
+        # Prints compressed sequence
 
     if input_type == 'file':
+        # Checks if the input corresponds to an existing file
+
         sequences = open(user_input, 'rt', encoding='UTF-8')
         for sequence in list(sequences):
             print(build_output(sequence))
+        # Reads file line by line
+        # Passes each line/sequence in to the build_ouput function
+        # Prints compressed sequence for each line/sequence
 
 
 # --------------------------------------------------
